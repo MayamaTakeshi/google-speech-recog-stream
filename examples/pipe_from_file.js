2de463783eaabd3e3d20580c3085e92bb2b41266
MayamaTakeshi <mayamatakeshi@gmail.com>
2024-05-06 08:51:00 +0900

    Updated examples

--- examples/pipe_from_file.js
+++ examples/pipe_from_file.js
@@ -3,7 +3,22 @@ const wav = require('wav')
 
 const GSRS = require('../index.js')
 
-const file = fs.createReadStream('./examples/artifacts/ohayou_gozaimasu.wav')
+const usage = () => {
+  console.log(`
+Arguments: language file_path
+Ex:        en-US hello_world.wav
+`)
+}
+
+if(process.argv.length != 4) {
+  usage()
+  process.exit(1)
+}
+
+language = process.argv[2]
+file_path = process.argv[3]
+
+const file = fs.createReadStream(file_path)
 const reader = new wav.Reader()
 
 reader.on('format', function (format) {
@@ -11,7 +26,7 @@ reader.on('format', function (format) {
   const sr = new GSRS({
     format,
     params: {
-      language: 'ja-JP',
+      language,
     }
   })
 
